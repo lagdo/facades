52da9b172b6fb6d4c95a79066b0a082f0b5ce026
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-03-30 04:09:18 +0200

    Add source files.

---
+++ src/ContainerWrapper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lagdo\Facades;
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+final class ContainerWrapper
+{
+    /**
+     * @var ContainerInterface
+     */
+    private static $container = null;
+
+    /**
+     * @param ContainerInterface $container
+     *
+     * @return void
+     */
+    public static function setContainer(ContainerInterface $container): void
+    {
+        self::$container = $container;
+    }
+
+    /**
+     * Get a service using the container.
+     *
+     * @param string $serviceId
+     *
+     * @return mixed
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function getFacadeService(string $serviceId): mixed
+    {
+        return self::$container->get($serviceId);
+    }
+}
